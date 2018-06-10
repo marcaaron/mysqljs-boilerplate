5dcfbe9f8fbe13e1b8c0d9316b4251e7963396d1
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-06-10 00:09:28 -0700

    add form and ejs

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const faker = require('faker');
 const mysql = require('mysql');
 const pretty = require('js-object-pretty-print').pretty;
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+// Error Print
+function handleError(err){
+  return `SQL attempted: ${err.sql}\nError Returned: ${err.sqlMessage}`;
+};
 
 // Open MySQL Connection
 const connection = mysql.createConnection({
@@ -21,7 +29,21 @@ app.get('/', (req, res) => {
   const q = 'SELECT COUNT(*) as user_count FROM users';
   connection.query(q, function(err, results){
     if(err) throw err;
-    res.send(`We have ${results[0].user_count} users.`);
+    const count = results[0].user_count;
+    res.render('home', {count});
+  });
+});
+
+// Create New User
+app.post('/register', (req, res)=> {
+  const person = {email: req.body.email };
+  connection.query('INSERT INTO users SET ?', person, function(err,result){
+    if(err){
+      res.send(handleError(err));
+    }else{
+      console.log(pretty(result));
+      res.redirect('/');
+    }
   });
 });
 
